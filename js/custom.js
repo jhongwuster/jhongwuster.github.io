1b795f72ee2420e3fdfe2f61b5ecd986750a4387
jhongwuster <jhongwuster@gmail.com>
2016-04-17 17:06:11 +1000

    Add go-up button

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+    
+// to top
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('.go-up').fadeIn();
+        } else {
+            $('.go-up').fadeOut();
+        }
+    });
+    $('.go-up').on('click', function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+})(jQuery); //end
