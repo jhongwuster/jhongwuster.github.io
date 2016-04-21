54821274ecf888e4c84f7b04e5cac0c0e113f420
jhongwuster <jhongwuster@gmail.com>
2016-04-21 21:47:31 +1000

    Fix stories css issue

--- js/img-filter.js
+++ js/img-filter.js
@@ -1,35 +1,51 @@
 (function ($) {
     "use strict";
+	var columnSize = function(){
+		var columns;
+		var width = $(window).width();
+        if($(window).width() <= 768) {
+			columns = 2;
+		} else {
+			columns = 3;
+		}
+		console.log("columnSize = " + columns);
+		return 3;
+	};
+	
 	$(document).ready(function () {
 
 	$(window).load(function() {
 		var $container = $('#work-div');
 $container.isotope({
 filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false,
-}
-});
+resizable: false, // disable normal resizing
+  // set columnWidth to a percentage of container width
+  masonry: { columnWidth: $container.width() / 3 }
+}).isotope('reLayout');
 
 
 $('.caegories a').click(function () {
 $('.caegories .active').removeClass('active');
 $(this).addClass('active');
 var selector = $(this).attr('data-filter');
+
 $container.isotope({
 filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
+resizable: false, // disable normal resizing
+  // set columnWidth to a percentage of container width
+  masonry: { columnWidth: $container.width() / columnSize() }
 });
 return false;
 });
+
+$(window).smartresize(function(){
+	
+	$container.isotope({
+  // set columnWidth to a percentage of container width
+  masonry: { columnWidth: $container.width() / columnSize() }
 	});
 	
 	});
-	
+	});
+	});
 })(jQuery); //end
